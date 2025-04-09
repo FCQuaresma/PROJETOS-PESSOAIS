07e349ff57daf79a984703bd63574775a2d4d5aa
Felipe Quaresma <felipecq62@gmail.com>
2025-04-08 22:33:53 -0300

    inertface grafica

---
+++ tkinter/Dandointerfacegrafica.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+raiz = Tk()
+
+#Criando uma ferramenta de rotulo
+
+meurotulo1 = Label(raiz, text="Interface Grafica" ).grid(row=0, column=0)
+meurotulo2 = Label(raiz, text="Meu nome é FELIPE CHAVES QUARESMA e amo programar" ).grid(row=2, column=2)
+
+
+
+# Empurrando para a tela e posicionando textos
+
+#meurotulo1.grid(row=0, column=0)
+#meurotulo2.grid(row=2, column=2)
+
+
+
+raiz.mainloop()
+
